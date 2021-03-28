22e5b449317d67cdc0a4c7b605b5e3d16f5463d2
NeuralLine <neurallines>
2021-03-28 20:08:18 +0100

    python training script

---
+++ README.md
@@ -0,0 +1,3 @@
+#Python exercise.
+
+Learning Python script
---
+++ app.py
@@ -0,0 +1,9 @@
+print("Welcome to my Python script")
+name = input("what is your name? ")
+
+birthYear = input("Hello " + name.upper() + " please enter your birth year: ")
+age = 2021 - int(birthYear)
+print("Great you are", age, "years old")
+first = input("First: ")
+second = input("Second: ")
+print(int(first) + int(second))
